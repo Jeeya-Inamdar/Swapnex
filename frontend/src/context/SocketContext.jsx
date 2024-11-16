67300782582273cec74c5f057cb9342d5290b799
A-bit-123 <a3681156@gmail.com>
2024-11-16 14:16:24 +0530

    update dependencies and refactor route middleware for consistency

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -5,29 +5,35 @@ import userAtom from "../atoms/userAtom";
 
 const SocketContext = createContext();
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useSocket = () => {
-	return useContext(SocketContext);
+  return useContext(SocketContext);
 };
 
 export const SocketContextProvider = ({ children }) => {
-	const [socket, setSocket] = useState(null);
-	const [onlineUsers, setOnlineUsers] = useState([]);
-	const user = useRecoilValue(userAtom);
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const user = useRecoilValue(userAtom);
 
-	useEffect(() => {
-		const socket = io("/", {
-			query: {
-				userId: user?._id,
-			},
-		});
+  useEffect(() => {
+    const socket = io("/", {
+      query: {
+        userId: user?._id,
+      },
+    });
 
-		setSocket(socket);
+    setSocket(socket);
 
-		socket.on("getOnlineUsers", (users) => {
-			setOnlineUsers(users);
-		});
-		return () => socket && socket.close();
-	}, [user?._id]);
+    socket.on("getOnlineUsers", (users) => {
+      setOnlineUsers(users);
+    });
+    return () => socket && socket.close();
+  }, [user?._id]);
 
-	return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
+  // console.log(onlineUsers, "Online users");
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
