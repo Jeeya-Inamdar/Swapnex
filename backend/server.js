4539cd9138deec72a47076a258de8fc38d3f2392
developer-alluvium <intern@novusha.com>
2026-05-10 17:03:36 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
 import job from "./cron/cron.js";
-import seedRoutes from "./routes/seedRoutes.js";
+// import seedRoutes from "./routes/seedRoutes.js";
 import errorHandler from "./middlewares/errorHandler.js";
 
 dotenv.config();
@@ -45,10 +45,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV !== "production") {
-  app.use("/api/seed", seedRoutes);
-  console.log("Seed route enabled");
-}
+// if (process.env.NODE_ENV !== "production") {
+//   app.use("/api/seed", seedRoutes);
+//   console.log("Seed route enabled");
+// }
 
 app.use((req, res, next) => {
   res.status(404).json({ error: "Route not found" });
